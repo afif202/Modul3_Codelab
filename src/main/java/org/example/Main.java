3fab4e2c9ff633fece1040402cad5dd92c676243
Afif Amhar <afifattamimi@gmail.com>
2024-10-23 13:43:22 +0700

    Added a simple Java program

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        String text = "Hello, world!";
+        String text = "Hello, git!";
 
         System.out.println(text);
     }
